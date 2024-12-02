9413c22a0a6c1e705141328f260d94f351156294
Pedro Luis <pedroluisnr22@gmail.com>
2024-12-01 21:22:00 -0500

    feat: :sparkles: add basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infallible;
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
+
+async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from("Hello, Rust HTTP Server!"))
+        .unwrap();
+    Ok(response)
+}
+
+#[tokio::main]
+async fn main() {
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
+
+    let address = ([127, 0, 0, 1], 8080).into();
+    let server = Server::bind(&address).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
