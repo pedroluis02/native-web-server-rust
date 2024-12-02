e690066759240c356f27ac7ed39e91adf0c88108
Pedro Luis <pedroluisnr22@gmail.com>
2024-12-01 22:02:05 -0500

    feat: :sparkles: add new server version

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 use std::convert::Infallible;
+use std::net::SocketAddr;
 
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server, StatusCode};
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 
-async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handle_request(
+    _req: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    let body = Full::new(Bytes::from("Hello, Rust HTTP Server with v1.5!"));
     let response = Response::builder()
         .status(StatusCode::OK)
-        .body(Body::from("Hello, Rust HTTP Server!"))
+        .body(body)
         .unwrap();
     Ok(response)
 }
 
 #[tokio::main]
-async fn main() {
-    let make_svc =
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let address = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(address).await?;
 
-    let address = ([127, 0, 0, 1], 8080).into();
-    let server = Server::bind(&address).serve(make_svc);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(handle_request))
+                .await
+            {
+                eprintln!("server error: {:?}", err);
+            }
+        });
     }
 }
